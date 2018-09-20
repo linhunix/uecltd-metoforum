3da40d1c3ecf0aabe778fa7fa9ee36e883e07611
Andrea Morello UEC <andrea.morello@unix.team>
2018-09-20 09:15:57 +0200

    First Angular and Material changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
+import { HttpClientModule }    from '@angular/common/http';
+import { ln4Manager } from '../ln4/ln4.module';
+import { MenuComponent } from './menu/menu.component';
+import { TabComponent } from './tabs/tab.component';
+import { ForumComponent } from './forum.component';
+import { NewsComponent } from './news.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    TabComponent,
+    ForumComponent,
+    NewsComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTabsModule
+    ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    MenuComponent,
+    TabComponent,
+    ForumComponent,
+    NewsComponent
+  ]
 })
-export class AppModule { }
+export class AppModule { 
+  public constructor (private http: HttpClientModule){
+    this.getLn4Manager().setService("http",http);
+  }
+  public getLn4Manager():ln4Manager{
+    return ln4Manager.GetInstance();
+  }
+}
