e1a093731f2267736e12be7230111b064c262d8a
Andrea Morello UEC <andrea.morello@unix.team>
2018-11-14 08:26:12 +0100

    Fix 1 part

--- src/app/Edit/edit.component.ts
+++ src/app/Edit/edit.component.ts
@@ -1,11 +1,18 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, Input } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { ln4Manager } from 'src/ln4/ln4.Manager';
+import { ln4A2SimpleComp } from 'src/ln4/ln4.A2SimpleComp';
 @Component({
     selector: 'editor-ln4',
     templateUrl:"edit.component.html",
 })
 export class EditorComponent {
+    @Input("forum")forum:string;
+    @Input("forumList")forumList:string;
+    @Input("topic")topic:string;
+    @Input("from")from:string;
+    @Input("subject")subject:string;
+    @Input("content")content:string;
     constructor(
         public dialogRef: MatDialogRef<EditorComponent>,
         @Inject(MAT_DIALOG_DATA) public data: any) { }
